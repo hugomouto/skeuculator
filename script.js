d9b5b3fa05bc8e1b646cfae0dc48e4c33ca819ba
Hugo Mouto <hmoutol@gmail.com>
2021-04-15 17:27:30 -0300

    feat: clicking on operation buttons makes them appear on screen

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-// Get Calc Buttons
-const calcButtons = document.getElementById('calc-buttons')
+// Get Calc Number and Function Buttons
+const calcNumButtons = document.getElementById('calc-buttons')
+// Get Operation Buttons
+const calcOpButtons = document.getElementById('graidient-buttons')
 //Get On Screen operation
 const onScreenOp = document.getElementById('calc-on-screen')
 
-//// Type buttons
-calcButtons.addEventListener ('click', function(event) {
+//// Type Numbers and Functions
+calcNumButtons.addEventListener ('click', function(event) {
   let keyEntry = event.target.innerText;
   if (onScreenOp.innerHTML === '0') {
     onScreenOp.innerHTML = '';
     onScreenOp.innerHTML = keyEntry;
   } else {
     onScreenOp.innerHTML = onScreenOp.innerHTML + keyEntry;
+  }
+})
+
+//// Type Operations
+calcOpButtons.addEventListener ('click', function(event) {
+  let keyEntry = event.target.innerText;
+  switch (true) {
+    case keyEntry === '+':
+    onScreenOp.innerHTML = onScreenOp.innerHTML + ' ' + keyEntry + ' ';
+    break;
+    case keyEntry === '-':
+    onScreenOp.innerHTML = onScreenOp.innerHTML + ' ' + keyEntry + ' ';
+    break;
+    case keyEntry === 'x':
+    onScreenOp.innerHTML = onScreenOp.innerHTML + ' ' + keyEntry + ' ';
+    break;
+    case keyEntry === '÷':
+    onScreenOp.innerHTML = onScreenOp.innerHTML + ' ' + keyEntry + ' ';
+    break;
   }
 })
